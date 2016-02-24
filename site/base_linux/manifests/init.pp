325a29d72c4d31a70c51f5c3189b3b03ae25aa53
Byron Jones <bjones@jadedrazor.com>
2016-02-24 12:13:12 +0000

    MOTD

--- site/base_linux/manifests/init.pp
+++ site/base_linux/manifests/init.pp
@@ -3,21 +3,9 @@ class base_linux {
   # I suggest using this file to control common linux box properties
   # such as, user keys, monitoring, s3cmd, then switch out to the project after done
 
+  include motd
+
   motd::register{'base_linux':}
   motd::register{"puppet_environment" : content => "Puppet Environment: $::environment"}
 
-
-  # Include the common linux module
-#  class { 'common_linux': }
-
-
-  # Nagios Agent configuration not wanted on vagrant boxes
-  if $::virtual != 'virtualbox' {
-    class {'kaplan_check_mk::agent':
-      require => [
-        Class["common_linux"],
-        Class["kaplan_project_${::kap_proj}"]
-      ]
-    }
-  }
 }
