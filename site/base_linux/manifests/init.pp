aa5a937035b5d1c7530360b5658514b9885e9ebf
Byron Jones <bjones@jadedrazor.com>
2016-02-24 18:58:14 +0000

    arrayzzzz

--- site/base_linux/manifests/init.pp
+++ site/base_linux/manifests/init.pp
@@ -1,24 +1,21 @@
 class base_linux {
 
-  # I suggest using this file to control common linux box properties
-  # such as, user keys, monitoring, s3cmd, then switch out to the project after done
-
   include motd
 
   motd::register{'base_linux':}
   motd::register{"puppet_environment" : content => "Puppet Environment: $::environment"}
 
   #make some groups
-  $osgroups = hiera('groups')
+  $osgroups = hiera_array('groups')
+
+
   $osgroups.each |String $osgroup| {
       group { "$osgroup":
         name     => "$osgroup",
         ensure   => present,
       }
   }
-  # Include devops users on all machines for the moment.
-  users { all:  }
 
-  # Create a devops group which we can then assign users to
+  users { all:  }
 
 }
